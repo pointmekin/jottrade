4e8b3db06291247e7560bdacc95757264167102b
Dhanabordee Mekintharanggur <pointmekin@users.noreply.github.com>
2025-12-16 01:24:40 +0700

    wip: auth and unauth sidebar state

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -3,67 +3,82 @@ import {
   Scripts,
   createRootRouteWithContext,
   useLocation,
-} from '@tanstack/react-router'
-import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
-import { TanStackDevtools } from '@tanstack/react-devtools'
+} from "@tanstack/react-router";
+import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools";
+import { TanStackDevtools } from "@tanstack/react-devtools";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 
-import Header from '../components/Header'
+import Header from "../components/Header";
 
-import TanStackQueryDevtools from '../integrations/tanstack-query/devtools'
+import TanStackQueryDevtools from "../integrations/tanstack-query/devtools";
 
-import appCss from '../styles.css?url'
+import appCss from "../styles.css?url";
 
-import type { QueryClient } from '@tanstack/react-query'
+import type { QueryClient } from "@tanstack/react-query";
+import { AppSidebar } from "@/components/app-sidebar";
 
 interface MyRouterContext {
-  queryClient: QueryClient
+  queryClient: QueryClient;
 }
 
 export const Route = createRootRouteWithContext<MyRouterContext>()({
   head: () => ({
     meta: [
       {
-        charSet: 'utf-8',
+        charSet: "utf-8",
       },
       {
-        name: 'viewport',
-        content: 'width=device-width, initial-scale=1',
+        name: "viewport",
+        content: "width=device-width, initial-scale=1",
       },
       {
-        title: 'TanStack Start Starter',
+        title: "TanStack Start Starter",
       },
     ],
     links: [
       {
-        rel: 'stylesheet',
+        rel: "stylesheet",
         href: appCss,
       },
     ],
   }),
 
   shellComponent: RootDocument,
-})
+});
 
 function RootDocument({ children }: { children: React.ReactNode }) {
-  const location = useLocation()
-  const hideHeaderRoutes = ['/sign-in', '/sign-up', '/dashboard']
-  const shouldHideHeader = hideHeaderRoutes.some(route => location.pathname.startsWith(route))
+  const location = useLocation();
+
+  // For specifying routes where the header should be hidden
+  const hideSidebarRoutes = ["/sign-in", "/sign-up"];
+  const shouldHideSidebar = hideSidebarRoutes.some((route) =>
+    location.pathname.startsWith(route)
+  );
 
   return (
     <html lang="en">
       <head>
         <HeadContent />
       </head>
-      <body>
-        {!shouldHideHeader && <Header />}
-        {children}
+      <body className="bg-background">
+        {/* Tanstack Demo Header */}
+        {/* {!shouldHideHeader && <Header />} */}
+        <SidebarProvider>
+          {!shouldHideSidebar && (
+            <>
+              <AppSidebar />
+              <SidebarTrigger />
+            </>
+          )}
+          <div className="w-full">{children}</div>
+        </SidebarProvider>
         <TanStackDevtools
           config={{
-            position: 'bottom-right',
+            position: "bottom-right",
           }}
           plugins={[
             {
-              name: 'Tanstack Router',
+              name: "Tanstack Router",
               render: <TanStackRouterDevtoolsPanel />,
             },
             TanStackQueryDevtools,
@@ -72,5 +87,5 @@ function RootDocument({ children }: { children: React.ReactNode }) {
         <Scripts />
       </body>
     </html>
-  )
+  );
 }
